92aea3d958e71769a5b84c02ccde4e9815701252
NamaWorks <pedronayamartinez@hotmail.com>
2024-09-12 21:47:14 +0200

    add connection to mongoDB using mongoose

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+require("dotenv").config();
 const express = require("express");
+const { connectDB } = require("./src/config/db");
 
+connectDB()
 
 const server = express();
+
+server.use("*", (req, res, next)=>{
+  return res.status(404).json("route not found")
+})
 
 server.listen(3000,()=>{
   console.log("server started at: https://localhost:3000")
---
+++ src/config/db.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URL)
+    console.log("connecting to DB successful")
+  } catch (error) {
+    console.log("error connecting to DB")
+    console.log(error)
+  }
+}
+
+module.exports = { connectDB }
