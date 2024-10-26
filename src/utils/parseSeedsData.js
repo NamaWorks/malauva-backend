48166fb60b409c97a9e62aea3ade4d18331dd772
NamaWorks <pedronayamartinez@hotmail.com>
2024-10-26 16:29:37 +0200

    add status to purchase model

--- src/utils/parseSeedsData.js
+++ src/utils/parseSeedsData.js
@@ -15,7 +15,6 @@ const parseSeedUsers = (data) => {
       this.lastConnection = lastConnection ? lastConnection : new Date()
       this.role= role == "admin" ? role = "admin" : role = "user"
       this.scoresGiven = scoresGiven.replace("\r", "").split(" ")
-      this.status = status
     }
   }
 
@@ -23,7 +22,7 @@ const parseSeedUsers = (data) => {
 
   for (let i = 1; i < arr.length; i++) {
     const itemInfo = arr[i].split(";")
-    usersArr.push(new user(itemInfo[0], itemInfo[1], itemInfo[2], itemInfo[3], itemInfo[4], itemInfo[5], itemInfo[6], itemInfo[7], itemInfo[8], itemInfo[9], itemInfo[10], itemInfo[11], itemInfo[12] || "processing"))
+    usersArr.push(new user(itemInfo[0], itemInfo[1], itemInfo[2], itemInfo[3], itemInfo[4], itemInfo[5], itemInfo[6], itemInfo[7], itemInfo[8], itemInfo[9], itemInfo[10], itemInfo[11]))
   }
   return usersArr
 }
@@ -59,7 +58,7 @@ const parseSeedPurchases = (data) => {
   const arr = data.split("\n")
   
   class purchase {
-    constructor (idNumber, client, itemsBought, totalSpent,  paymentMethod, adress, vatNumber, date) {
+    constructor (idNumber, client, itemsBought, totalSpent,  paymentMethod, adress, vatNumber, date, status) {
       this.idNumber=idNumber
       this.client=client
       this.itemsBought = itemsBought
@@ -68,6 +67,7 @@ const parseSeedPurchases = (data) => {
       this.adress = adress
       this.vatNumber = vatNumber=="TRUE" ? true : false
       this.date = date
+      this.status = status || "processing"
     }
   }
 
@@ -75,7 +75,7 @@ const parseSeedPurchases = (data) => {
 
   for (let i = 1; i < arr.length; i++) {
     const itemInfo = arr[i].split(";")
-    purchasesArr.push(new purchase(itemInfo[0], itemInfo[1], itemInfo[2], itemInfo[3], itemInfo[4], itemInfo[5], itemInfo[6], itemInfo[7]))
+    purchasesArr.push(new purchase(itemInfo[0], itemInfo[1], itemInfo[2], itemInfo[3], itemInfo[4], itemInfo[5], itemInfo[6], itemInfo[7], itemInfo[8]))
   }
   return purchasesArr
 }
