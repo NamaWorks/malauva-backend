7ff4dcb137660f9596fc98d719f8bf26f8a5c073
NamaWorks <pedronayamartinez@hotmail.com>
2025-01-07 18:40:50 +0100

    change token duration

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateSign = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1y" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "2h" });
 };
 
 const verifyJwt = (token) => {
