14c8b50a141570b2a876a2640633773ecf48d920
NamaWorks <pedronayamartinez@hotmail.com>
2025-02-12 20:51:12 +0100

    change the duration of the JWT to 1 day

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require("jsonwebtoken");
 
 const generateSign = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "4h" });
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
 };
 
 const verifyJwt = (token) => {
