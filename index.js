bcf49dae60bf008e9e6848cadb176c204fff3655
NamaWorks <pedronayamartinez@hotmail.com>
2025-03-17 21:03:47 +0100

    update cors

--- index.js
+++ index.js
@@ -15,20 +15,12 @@ const server = express();
 connectDB();
 server.use(express.json())
 // with the next line we allow our server to use cross origin resources
-server.use(cors(
-  {
-    origin: 'http://localhost:5173', // Replace with your frontend's origin
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204
-  },
-  {
-    origin: 'https://malauva-frontend.vercel.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-    optionsSuccessStatus: 204
-  },
-));
+server.use(cors({
+  origin: ['http://localhost:5173', 'https://malauva-frontend.vercel.app'], // Replace with your frontend's origins
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+  optionsSuccessStatus: 204
+}));
 // with the next line we allow our app to read .json data
 server.use(express.json());
 
